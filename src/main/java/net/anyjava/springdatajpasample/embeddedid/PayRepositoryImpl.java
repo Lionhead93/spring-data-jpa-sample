8ad325b01bce1a37b1cd0b88d9d77d0db1ecb1cd
anyjava <sht21c@gmail.com>
2019-01-02 09:21:32 +0900

    QeuryDsl JPAQueryFactory 를 사용하도록 리팩토링

--- src/main/java/net/anyjava/springdatajpasample/embeddedid/PayRepositoryImpl.java
+++ src/main/java/net/anyjava/springdatajpasample/embeddedid/PayRepositoryImpl.java
@@ -1,25 +1,30 @@
 package net.anyjava.springdatajpasample.embeddedid;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static net.anyjava.springdatajpasample.embeddedid.QPay.pay;
+import static net.anyjava.springdatajpasample.embeddedid.QPayShop.payShop;
+
 @Repository
 public class PayRepositoryImpl extends QuerydslRepositorySupport implements PayRepositoryCustom {
 
-    private static final QPay pay = QPay.pay;
-    private static final QPayShop payShop = QPayShop.payShop;
+    private final JPAQueryFactory jpaQueryFactory;
 
-    public PayRepositoryImpl() {
+    public PayRepositoryImpl(JPAQueryFactory jpaQueryFactory) {
         super(Pay.class);
+        this.jpaQueryFactory = jpaQueryFactory;
     }
 
     @Override
     public List<Pay> findAllByShopName(String shopName) {
-        return from(pay)
+        return jpaQueryFactory.from(pay)
                 .innerJoin(pay.payShops, payShop)
                 .where(payShop.shopName.contains(shopName))
+                .select(pay)
                 .fetch();
     }
 }
--- src/main/java/net/anyjava/springdatajpasample/idclass/Pay2RepositoryImpl.java
+++ src/main/java/net/anyjava/springdatajpasample/idclass/Pay2RepositoryImpl.java
@@ -1,25 +1,30 @@
 package net.anyjava.springdatajpasample.idclass;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static net.anyjava.springdatajpasample.idclass.QPay2.pay2;
+import static net.anyjava.springdatajpasample.idclass.QPayShop2.payShop2;
+
 @Repository
 public class Pay2RepositoryImpl extends QuerydslRepositorySupport implements Pay2RepositoryCustom {
 
-    private static final QPay2 pay2 = QPay2.pay2;
-    private static final QPayShop2 payShop2 = QPayShop2.payShop2;
+    private final JPAQueryFactory jpaQueryFactory;
 
-    public Pay2RepositoryImpl() {
+    public Pay2RepositoryImpl(JPAQueryFactory jpaQueryFactory) {
         super(Pay2.class);
+        this.jpaQueryFactory = jpaQueryFactory;
     }
 
     @Override
     public List<Pay2> findAllByShopName(String shopName) {
-        return from(pay2)
+        return jpaQueryFactory.from(pay2)
                 .innerJoin(pay2.payShop2List, payShop2)
                 .where(payShop2.shopName.contains(shopName))
+                .select(pay2)
                 .fetch();
     }
 }
